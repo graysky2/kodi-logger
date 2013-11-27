74dcc9300301a97bf898a93a0feb834538c8a158
graysky <graysky@archlinux.us>
2013-11-27 09:21:06 -0500

    now works with xbmc on x86 or ARM

--- xbmc-logger.pl
+++ xbmc-logger.pl
@@ -1,11 +1,27 @@
 #!/usr/bin/perl
+
+# xbmc-logger version 1.1
+#
+# CHANGELOG
+# v1.0  Initial release
+# V1.1  Now works with xbmc on x86 or ARM
+
 use warnings;
 use strict;
 use File::Copy;
 use DB_File;
 
+# this is the directory where xbmc.log is expected
 my $w_dir = "$ENV{HOME}/.xbmc/temp";
+
+# this is the dir where you wish to write out the xmbc-watched.log
+# if you want it to be something outside of the xbmc users home dir like
+# /var/log for example, that location must have an empty xbmc-watched.log
+# present and owned by the xbmc user:group or else this script will fail
+# to write to the log
 my $l_dir = "$ENV{HOME}/.xbmc";
+
+# this is the dir where a temp db is kept
 my $t_dir = '/tmp';
 my $now = localtime();
 
@@ -22,7 +38,7 @@ open my $fh, '<', $fn
 
 while (my $line = <$fh>) {
 	chomp $line;
-	if ($line =~ m/^(\S+)\s.*NOTICE: DVDPlayer: Opening: (.*)/) {
+	if ($line =~ m/^(\S+)\s.*Player: Opening: (.*)/) {
 		my $ts = $1;
 		my $dn = $2;
 		if (!$seen{$ts}) {
